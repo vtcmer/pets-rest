1f3b1242853b1cdec565161d9bcd7c91206123b9
vtcmer <vtcmer@yahoo.com>
2018-07-30 16:08:57 +0200

    Consulta por species

--- src/main/java/com/sinz/pets/service/PetsRepository.java
+++ src/main/java/com/sinz/pets/service/PetsRepository.java
@@ -2,8 +2,17 @@ package com.sinz.pets.service;
 
 import com.sinz.pets.model.Pet;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PetsRepository extends MongoRepository<Pet,String> {
+
+    @Query("{species : ?0}")
+    List<Pet> findBySpeciesNameQuery(final String speciesName);
+
+    @Query("{species : ?0, breeds: ?1}")
+    List<Pet> findBySpeciesAndBreedsQuery(final String species, final String breeds);
 }
