28c8813d759bc99248287df447656db9c01f8b73
vtcmer <vtcmer@gmail.com>
2018-08-01 22:41:26 +0200

    Update PetsApplication.java

--- src/main/java/com/sinz/pets/PetsApplication.java
+++ src/main/java/com/sinz/pets/PetsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PetsApplication {
-	// -- nueva rama del token
+	// -- nueva rama del token modificada
 	public static void main(String[] args) {
 		SpringApplication.run(PetsApplication.class, args);
 	}
